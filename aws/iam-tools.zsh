7cc9bfff0e65109d5df2cedb226aaa736e747cf7
larkin <pete@getchute.com>
2013-05-23 09:38:06 -0700

    add aws-iam-tools config

---
+++ aws/iam-tools.zsh
@@ -0,0 +1,5 @@
+# configure IAM tools for brew package aws-iam-tools
+
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export AWS_IAM_HOME="/usr/local/opt/aws-iam-tools/jars"
+export AWS_CREDENTIAL_FILE=$HOME/.aws-credentials-master
